b267df9b8e1769b364ab1faf7a6f344722cfe84c
Vinícius Valle <tardelivinicius@gmail.com>
2022-01-07 21:45:12 -0300

    Added comment function

--- components/Post.js
+++ components/Post.js
@@ -1,8 +1,39 @@
 import { BookmarkIcon, ChatIcon, DotsHorizontalIcon, EmojiHappyIcon, HeartIcon, PaperAirplaneIcon } from "@heroicons/react/outline"
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from "firebase/firestore";
 import { useSession } from "next-auth/react"
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import Moment from 'react-moment';
+
 function Post({ id, username, userImg, img, caption}) {
     const { data: session } = useSession();
+    const [comment, setComment] = useState("");
     const [comments, setComments] = useState([]);
+
+    useEffect(
+        () =>
+            onSnapshot(
+                query(collection(db, 'posts', id, 'comments'), orderBy('timestamp', 'desc')), 
+                snapshot => {
+                    setComments(snapshot.docs);
+                }
+            ),
+        [db]
+    );
+
+    const sendComment = async (e) => {
+        e.preventDefault();
+
+        const commentToSend = comment;
+        setComment('');
+
+        await addDoc(collection(db, 'posts', id, 'comments'), {
+            comment: commentToSend,
+            username: session.user.username,
+            userImage: session.user.image,
+            timestamp: serverTimestamp(),
+        })
+    }
     return (
         <div className="bg-white my-7 border rounded-sm">
             {/* Header */}
@@ -34,13 +65,40 @@ function Post({ id, username, userImg, img, caption}) {
             </p>
 
             {/* Comments */}
+            {comments.length > 0 && (
+                <div className="ml-10 h2- overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+                    {comments.map(comment => (
+                        <div key={comment.id} className="flex items-center space-x-2 mb-3">
+                            <img className="h-7 rounded-full" src={comment.data().userImage} alt="" />
+                            <p className="text-sm flex-1">
+                                <span className="font-bold">{comment.data().username}
+                                </span>{" "}
+                                {comment.data().comment}
+                                </p>
+                            <Moment interval={1000} fromNow className="pr-5 text-xs">
+                                { comment.data().timestamp?.toDate() }
+                            </Moment>
+                        </div>
+                    ))}
+                </div>
+            )}
 
             {/* input box */}
             {session && (
                 <form className="flex items-center p-4">
                     <EmojiHappyIcon className="h-7"/>
-                    <input type="text" placeholder="Add a comment..." className="border-none flex-1 focus:ring-0 outline-none" />
-                    <button className="font-semibold text-blue-400">Post</button>
+                    <input type="text" 
+                        value={comment}
+                        onChange={e => setComment(e.target.value)}
+                        placeholder="Add a comment..." 
+                        className="border-none flex-1 focus:ring-0 outline-none" 
+                    />
+                    <button type="submit" 
+                        disabled={!comment.trim()} 
+                        onClick={sendComment}
+                        className="font-semibold text-blue-400">
+                        Post
+                    </button>
                 </form>
             )}
 
